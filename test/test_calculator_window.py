e9b549e2c70f027fc152f193a6a238e5910fc9b5
Landry Simo <landrysimo99@gmail.com>
2023-09-24 21:43:52 +0100

    setup requiremtns

---
+++ test/test_calculator_window.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class TestCalculatorWindow(TestCase):
+    def test_btn_clicked(self):
+        self.fail()
+
+    def test_set_correction(self):
+        self.fail()
+
+
+
